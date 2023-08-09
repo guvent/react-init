bb410b21c5dc926dc7656968f10ac5c2be46f832
Guven T <quvven@yahoo.de>
2023-08-09 18:26:49 +0300

    add example with redux store.

--- src/Layout/Navigation.tsx
+++ src/Layout/Navigation.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../Store/hooks';
+import { RootState, actions } from '../Store';
 
 export default function Navigation() {
+    const opened = useAppSelector((state: RootState) => state.mainMenu.status);
+    const dispatch = useAppDispatch();
+
+    const handleOpen = () => dispatch(actions.mainMenu.menuOpen(true));
+    const handleClose = () => dispatch(actions.mainMenu.menuOpen(false));
+
+    const handleRequest = () => {
+        console.warn('Requested....');
+    };
+
     return (
         <div className="main">
-            <div className="navigate">Navigation</div>
+            <div className="navigate">
+                <input type="button" value="Open" onClick={handleOpen} />
+                <input type="button" value="Close" onClick={handleClose} />
+                <input
+                    type="text"
+                    name="value"
+                    id="value"
+                    value={'' + opened}
+                />
+                <input type="button" value="Request" onClick={handleRequest} />
+            </div>
+            <hr />
+            <hr />
             <div className="container">
                 <Outlet />
             </div>
